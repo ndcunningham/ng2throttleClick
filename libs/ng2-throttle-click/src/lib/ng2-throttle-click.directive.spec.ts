9202a646c98117eedeebd9ff5a06fccd46b2f5af
ncunningham <ndcunningham@gmail.com>
2018-10-23 22:39:23 -0400

    (chore) - Correct unit test
    Meaning example of a unit test that higlights the usefulness of the directive

--- libs/ng2-throttle-click/src/lib/ng2-throttle-click.directive.spec.ts
+++ libs/ng2-throttle-click/src/lib/ng2-throttle-click.directive.spec.ts
@@ -1,10 +1,10 @@
-import { TestBed, ComponentFixture, fakeAsync } from '@angular/core/testing';
+import { TestBed, ComponentFixture, fakeAsync, async, tick, discardPeriodicTasks } from '@angular/core/testing';
 import { Component, DebugElement } from '@angular/core';
 import { By } from '@angular/platform-browser';
 import { Ng2ThrottleClickDirective } from './ng2-throttle-click.directive';
 
 @Component({
-  template: '<button ng2throttleClick (click)="testClick($event)">Test</button>'
+  template: '<button ng2throttleClick (throttleClick)="testClick($event)">Test</button>'
 })
 class TestThrottleClickComponent {
   clicks = 0;
@@ -33,8 +33,25 @@ describe('Ng2ThrottleClickDirective', () => {
     expect(directive).toBeTruthy();
   });
 
-  it('should allow click events', () => {
-    buttonEl.triggerEventHandler('click', null);
-    expect(component.clicks).toEqual(1);
-  });
+  it(
+    'should not allow multiple mouse down events within the default time 500ms',
+    fakeAsync(() => {
+      buttonEl.triggerEventHandler('mousedown', null);
+
+      tick(1);
+      fixture.detectChanges();
+      expect(component.clicks).toEqual(0);
+
+      buttonEl.triggerEventHandler('mousedown', null);
+      buttonEl.triggerEventHandler('mousedown', null);
+
+      tick(1);
+      fixture.detectChanges();
+
+      tick(498);
+      fixture.detectChanges();
+      expect(component.clicks).toEqual(1);
+      discardPeriodicTasks();
+    })
+  );
 });
